517fd71ab96b9c12cd9e893f3e8c657b6272cba7
rafael <rafaelmoura7033@gmail.com>
2023-01-19 14:19:53 -0300

    Creating Animal Interface

---
+++ hello-world/src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import Animal from '../../Animal';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent {
+  animals: Animal[] = [
+    {
+      name: "Toto", type: "Dog", age: 4
+    },
+    {
+      name: "Mia", type: "Cat", age: 10
+    },
+    {
+      name: "Zeca", type: "Bird", age: 2
+    }
+  ]
+
+  animalDetails: string = ''
+
+  showAge(animal: Animal) {
+    this.animalDetails = `The animal ${animal.name} is ${animal.age} years old!`;
+  }
+
+}
