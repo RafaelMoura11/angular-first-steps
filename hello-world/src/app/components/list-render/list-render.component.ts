66cc0e1ab9fbc9af505bd29ae56d4640f9057556
rafael <rafaelmoura7033@gmail.com>
2023-01-23 14:57:52 -0300

    Implementing routing module

--- hello-world/src/app/components/list-render/list-render.component.ts
+++ hello-world/src/app/components/list-render/list-render.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 
 import Animal from '../../Animal';
 
+import ListService from '../../'
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
+  constructor(private listService: ListService)
+
   animals: Animal[] = [
     {
       name: "Toto", type: "Dog", age: 4
